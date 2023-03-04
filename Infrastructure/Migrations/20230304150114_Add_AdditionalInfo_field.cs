6f55ec02cb9eb8ccd4ddca8eca90364eeff30e90
AdamBachorz <adam.bachorz1702@gmail.com>
2023-03-04 16:03:36 +0100

    Add AdditionalInfo field

---
+++ Infrastructure/Migrations/20230304150114_Add_AdditionalInfo_field.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Add_AdditionalInfo_field : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "AdditionalInfo",
+                table: "Foods",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.UpdateData(
+                table: "Foods",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "AdditionalInfo",
+                value: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AdditionalInfo",
+                table: "Foods");
+        }
+    }
+}
--- InfrastructureAbstraction/Entities/Food.cs
+++ InfrastructureAbstraction/Entities/Food.cs
@@ -4,16 +4,17 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace InfrastructureAbstractions.Entities;
 
-    public class Food : Entity
-    {
-        public string? RestaurantName { get; set; }
-        public string? FoodItems { get; set; }
-        public FoodState FoodState { get; set; } = FoodState.Open;
-        public DateTime? CreateDate { get; set; } = DateTime.Now.Date;
+public class Food : Entity
+{
+    public string? RestaurantName { get; set; }
+    public string? FoodItems { get; set; }
+    public FoodState FoodState { get; set; } = FoodState.Open;
+    public DateTime? CreateDate { get; set; } = DateTime.Now.Date;
+    public string AdditionalInfo { get; set; } = string.Empty;
 
-        [NotMapped]
-        public IList<string>? FoodItemsList => FoodItems?.Split(";")?.ToList();
-        [NotMapped]
-        public string FoodItemsDescription => $"({FoodItems?.Replace(";", ", ")/*.Substring(0, 20)*/}...)";
-    }
+    [NotMapped]
+    public IList<string>? FoodItemsList => FoodItems?.Split(";")?.ToList();
+    [NotMapped]
+    public string FoodItemsDescription => $"({FoodItems?.Replace(";", ", ")/*.Substring(0, 20)*/}...)";
+}
 

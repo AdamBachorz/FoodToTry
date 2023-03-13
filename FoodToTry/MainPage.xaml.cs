9d11e631dee42041142ddaef86495e4dfb1d34cb
AdamBachorz <adam.bachorz1702@gmail.com>
2023-03-13 17:27:30 +0100

    Add Internet connection function check

--- FoodToTry/MainPage.xaml.cs
+++ FoodToTry/MainPage.xaml.cs
@@ -16,7 +16,6 @@ public partial class MainPage : ContentPage
 	private async void CheckBox_CheckedChanged(object sender, CheckedChangedEventArgs e)
 	{
 		var referenceObject = Utils.GetObjectFromControl<CheckBox, Food>(sender);
-
         await ViewModel.OnCheckboxChange(referenceObject, e.Value);
 	}
 }
--- FoodToTry/Utils.cs
+++ FoodToTry/Utils.cs
@@ -10,6 +10,30 @@ namespace FoodToTry
 {
     public static class Utils
     {
+        public static bool IsInternet()
+        {
+            return Connectivity.Current.NetworkAccess == NetworkAccess.Internet;
+        }
+        
+        public static void InvokeIfInternetIsOn(Action action, Action actionIfNoInternet = null)
+        {
+            if (Connectivity.Current.NetworkAccess == NetworkAccess.Internet)
+            {
+                action();
+            }
+            else
+            {
+                if (actionIfNoInternet is null)
+                {
+                    Shell.Current.DisplayAlert("Uh Oh!", "No Internet", "OK");
+                }
+                else
+                {
+                    actionIfNoInternet();
+                }
+            }
+        }
+
         public static T GetObjectFromControl<C, T>(object sender) where C : View where T : class
         {
             return ((C)sender).BindingContext as T;
